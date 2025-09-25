c74878cdcefbf9c5b2221e1ec7047e01ef5fcf35
sheidebr <sheidebr@CB9444>
2025-09-25 17:17:57 -0500

    Move I PASSED button to countdown banner - only show after scheduled test date

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -1,19 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using CisspTrainingApp.Models;
 
 namespace CisspTrainingApp.Pages;
 
 public class DashboardModel : PageModel
 {
     private readonly ILogger<DashboardModel> _logger;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public DashboardModel(ILogger<DashboardModel> logger)
+    public DashboardModel(ILogger<DashboardModel> logger, UserManager<ApplicationUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
 
-    public void OnGet()
-    {
+    public bool ShowPassedButton { get; set; }
 
+    public async Task OnGetAsync()
+    {
+        if (User.Identity.IsAuthenticated)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            ShowPassedButton = user?.ScheduledTestDate.HasValue == true && 
+                              user.ScheduledTestDate.Value < DateTime.Now;
+        }
     }
 }
--- obj/Debug/net9.0/CisspTrainingApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CisspTrainingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CisspTrainingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d3d7f2a5bb4fd69c6649add6a90d1984f55c249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b07e0e5e05d358936f48a87ff9647060ebf72a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CisspTrainingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CisspTrainingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
